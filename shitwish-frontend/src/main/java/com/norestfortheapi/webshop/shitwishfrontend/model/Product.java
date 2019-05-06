ad69b10c141306e9f17ec36cfb4b8d6a58732a29
Nora Filep <filepnora@gmail.com>
2019-05-06 15:14:28 +0200

    product model with base fields

---
+++ shitwish-frontend/src/main/java/com/norestfortheapi/webshop/shitwishfrontend/model/Product.java
@@ -0,0 +1,21 @@
+package com.norestfortheapi.webshop.shitwishfrontend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imagePath;
+
+
+}
