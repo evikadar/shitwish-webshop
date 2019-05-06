f69c333c58e20684ac1818f361e2a672a57129f3
Nora Filep <filepnora@gmail.com>
2019-05-06 16:16:41 +0200

    productlist moved

--- shitwish-frontend/src/main/java/com/norestfortheapi/webshop/shitwishfrontend/controller/ShitwishFrontendController.java
+++ shitwish-frontend/src/main/java/com/norestfortheapi/webshop/shitwishfrontend/controller/ShitwishFrontendController.java
@@ -1,12 +1,13 @@
 package com.norestfortheapi.webshop.shitwishfrontend.controller;
 
 import com.norestfortheapi.webshop.shitwishfrontend.model.Product;
+import com.norestfortheapi.webshop.shitwishfrontend.service.ServiceCaller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -14,13 +15,12 @@ public class ShitwishFrontendController {
 
     private HttpSession session;
 
+    @Autowired
+    private ServiceCaller serviceCaller;
+
     @GetMapping("/")
     public String displayPage(Model model){
-        List<Product> products = new ArrayList<>();
-        Product firstCoffeeThingy = Product.builder().id(1L).name("First").price(10.00).description("Awesome").imagePath("1").build();
-        Product secondCoffeeThingy = Product.builder().id(2L).name("Second").price(10.00).description("Awesome").imagePath("2").build();
-        products.add(firstCoffeeThingy);
-        products.add(secondCoffeeThingy);
+        List<Product> products = serviceCaller.getProductList();
         model.addAttribute("products", products);
         return "index";
     }
--- shitwish-frontend/src/main/java/com/norestfortheapi/webshop/shitwishfrontend/service/ServiceCaller.java
+++ shitwish-frontend/src/main/java/com/norestfortheapi/webshop/shitwishfrontend/service/ServiceCaller.java
@@ -1,9 +1,47 @@
 package com.norestfortheapi.webshop.shitwishfrontend.service;
 
+import com.norestfortheapi.webshop.shitwishfrontend.model.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
+@Slf4j
 public class ServiceCaller {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${url.base}")
+    private String baseUrl;
+
+    @Value("${url.products}")
+    private String productUrl;
+
+    public List<Product> getProductList() {
+
+        try {
+            ResponseEntity<List<Product>> productList = restTemplate.exchange(baseUrl + productUrl, HttpMethod.GET,
+                    null, new ParameterizedTypeReference<List<Product>>(){});
+            return productList.getBody();
+        } catch (Exception e){
+            log.error(e.getMessage());
+            List<Product> products = new ArrayList<>();
+            Product firstCoffeeThingy = Product.builder().id(1L).name("First").price(10.00).description("Awesome").imagePath("1").build();
+            Product secondCoffeeThingy = Product.builder().id(2L).name("Second").price(10.00).description("Awesome").imagePath("2").build();
+            products.add(firstCoffeeThingy);
+            products.add(secondCoffeeThingy);
+            return products;
+        }
+
+    }
 
 }
