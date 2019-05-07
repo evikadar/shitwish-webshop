a9ba965cb590076e5ac2e4255fcbce4845ff5743
Akos Kolloros <kollorosa@gmail.com>
2019-05-07 15:03:23 +0200

    user model

---
+++ shitwish-frontend/src/main/java/com/norestfortheapi/webshop/shitwishfrontend/model/WishUser.java
@@ -0,0 +1,18 @@
+package com.norestfortheapi.webshop.shitwishfrontend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class WishUser {
+    long id;
+    String name;
+    String password;
+    String address;
+    String email;
+}
