457a8d94b4c8e3e9795494cd9315ad0c80c62685
Evi Kadar <evi.kadar@gmail.com>
2019-05-07 11:41:57 +0200

    basic product model and product category enum class created

--- products/src/main/java/com/norestfortheapi/webshop/products/model/Product.java
+++ products/src/main/java/com/norestfortheapi/webshop/products/model/Product.java
@@ -1,9 +1,17 @@
 package com.norestfortheapi.webshop.products.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Product {
 
     private Long id;
-
-
+    private String name;
+    private ProductCategory productCategory;
+    private double price;
+    private String description;
+    private String imagePath;
 
 }
--- products/src/main/java/com/norestfortheapi/webshop/products/service/ProductService.java
+++ products/src/main/java/com/norestfortheapi/webshop/products/service/ProductService.java
@@ -1,19 +1,20 @@
 package com.norestfortheapi.webshop.products.service;
 
 import com.norestfortheapi.webshop.products.model.Product;
+import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
-
 @Slf4j
 @Service
 public class ProductService {
 
     public Product getProduct() {
-        return null;
+        Product firstCoffeeThingy = Product.builder().id(1L).name("First").price(10.00).description("Awesome").imagePath("1").build();
+        return firstCoffeeThingy;
     }
 
 }
