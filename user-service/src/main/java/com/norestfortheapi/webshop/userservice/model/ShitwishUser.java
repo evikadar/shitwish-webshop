f33efa370b9ffefca86aa19708fdab297162922e
Nora Filep <filepnora@gmail.com>
2019-05-08 13:48:25 +0200

    model classes for user and address

---
+++ user-service/src/main/java/com/norestfortheapi/webshop/userservice/model/ShitwishUser.java
@@ -0,0 +1,31 @@
+package com.norestfortheapi.webshop.userservice.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Slf4j
+@Builder
+@Entity
+public class ShitwishUser {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String userName;
+    private String password;
+    private String email;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @JsonIgnoreProperties({"user"})
+    private UserAddress address;
+
+}
